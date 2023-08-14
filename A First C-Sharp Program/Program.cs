d0ebec5f6eee909629147e8e228af0851f8dfa93
dylanthesoldier17 <dylan.moore@section6.tech>
2023-08-14 14:30:26 +0900

    Completed Project 1
    Completed pages 2-4 in book. This completes chapter 1.

--- A First C-Sharp Program/Program.cs
+++ A First C-Sharp Program/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int x = 12 * 30;
+Console.WriteLine(x);
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine(FeetToInches(30));
+Console.WriteLine(FeetToInches(100));
+
+SayHello();
+
+int FeetToInches(int feet)
+{
+   return feet * 12;
+}
+
+void SayHello()
+{
+   Console.WriteLine("Hello World!");
+}
