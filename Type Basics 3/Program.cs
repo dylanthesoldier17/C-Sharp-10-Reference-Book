9059728ca22464eb677f3789de31e5b1da897c4b
dylanthesoldier17 <dylan.moore@section6.tech>
2023-08-18 11:03:51 +0900

    Added Panda implementation to Main

--- Type Basics 3/Program.cs
+++ Type Basics 3/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Type_Basics_3;
+
+Panda p1 = new Panda("steve");
+Panda p2 = new Panda("carl");
+
+Console.WriteLine($"Panda 1's name is {p1.Name}.");
+Console.WriteLine($"Panda 2's name is {p2.Name}.");
